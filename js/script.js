101c195333309bcc4a102c5573ac0d3597f8b355
test <test@MacBook-Pro-Malik.local>
2024-01-08 18:56:02 +0600

    init seacrh script part 2

--- js/script.js
+++ js/script.js
@@ -20,3 +20,50 @@ const actionBurger = document.querySelector('.action__burger'),
 
 
 actionBurger.addEventListener('click', () => burgerMenu.classList.toggle('active'))
+
+//скрипт для поиска 
+const searchInput = document.querySelector('.search__input'),
+      searchResult = document.querySelector('.search__result-list')
+
+
+const api = `
+https://gist.githubusercontent.com/dreamprogrammin/
+6fd488a88223921fccb0d0ec2e464e74/raw/61f96a1576f25eb3d393aa035b1f7b114946367c/data-kmf.json`
+
+const dataResults = []
+
+fetch(api)
+.then(res => res.json()
+.then(data => data.forEach(result => dataResults.push(result)))
+)
+
+function getResult (word, dataResults) {
+    return dataResults.filter(s=> {
+        const regex = new RegExp(word, 'gi')
+
+        return s.name.match(regex)
+    })
+}
+
+
+function displayResults () {
+
+    const dataResult = data => `<li class="search__result-item"><p class="result__item-data">${data}</p></li>`
+    
+    const results = getResult(this.value, dataResults)
+
+    const html = results.map(s=> {
+
+        const regex = new RegExp(this.value, 'gi')
+        const resultsName = s.name.replace(regex,`<strong class="font__weight">${this.value}</strong>`)
+
+        return `
+        <li class="search__result-item">${resultsName}</li>
+        `
+    }).join('')
+
+    searchResult.innerHTML = !this.value == '' ? dataResult(`Найдено ${results.length} результата поиска`) + html : ''
+}
+
+searchInput.addEventListener('change', displayResults)
+searchInput.addEventListener('keyup', displayResults)
